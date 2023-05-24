190f5030a52e7ed3beff0dde2809883a5e348b31
TheLukCraft <luk1997r@o2.pl>
2023-05-24 15:27:27 +0200

    neetcode

---
+++ MyProject.Tests/BmiCalculatorFacadeTests.cs
@@ -0,0 +1,28 @@
+﻿namespace MyProject.Tests
+{
+    public class BmiCalculatorFacadeTests
+    {
+        private const string OVERWEIGHT_SUMMARY = "You are a bit overweight";
+
+        [Fact]
+        public void GetResult_ForValidInputs_ReturnsCorrectResult()
+        {
+            // arrange
+
+            BmiCalculatorFacade bmiCalculatorFacade = new BmiCalculatorFacade(UnitSystem.Metric);
+
+            double weight = 90;
+            double height = 190;
+
+            // act
+
+            BmiResult result = bmiCalculatorFacade.GetResult(weight, height);
+
+            // assert
+
+            Assert.Equal(24.93, result.Bmi);
+            Assert.Equal(BmiClassification.Overweight, result.BmiClassification);
+            Assert.Equal(OVERWEIGHT_SUMMARY, result.Summary);
+        }
+    }
+}
