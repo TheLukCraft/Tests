a44fa611159241c81565fe12b7d9e1e805949f20
TheLukCraft <luk1997r@o2.pl>
2023-05-24 05:02:21 +0200

    Determinator testing

---
+++ MyProject.Tests/BmiDeterminatorTests.cs
@@ -0,0 +1,22 @@
+﻿namespace MyProject.Tests
+{
+    public class BmiDeterminatorTests
+    {
+        [Fact]
+        public void DetermineBmi_ForBmiBelow18_5_ReturnsUnderweightClassification()
+        {
+            // arrange
+
+            BmiDeterminator bmiDeterminator = new BmiDeterminator();
+            double bmi = 10;
+
+            // act
+
+            BmiClassification result = bmiDeterminator.DetermineBmi(bmi);
+
+            // assert
+
+            Assert.Equal(BmiClassification.Underweight, result);
+        }
+    }
+}
