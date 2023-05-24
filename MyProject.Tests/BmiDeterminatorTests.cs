1531113960813383d9deea2055eaa87ed7ea9803
TheLukCraft <luk1997r@o2.pl>
2023-05-24 05:16:48 +0200

    test parameterization

--- MyProject.Tests/BmiDeterminatorTests.cs
+++ MyProject.Tests/BmiDeterminatorTests.cs
@@ -2,6 +2,22 @@
 {
     public class BmiDeterminatorTests
     {
+        //[Fact]
+        //public void DetermineBmi_ForBmiBelow18_5_ReturnsUnderweightClassification()
+        //{
+        //    // arrange
+
+        //    BmiDeterminator bmiDeterminator = new BmiDeterminator();
+        //    double bmi = 10;
+
+        //    // act
+
+        //    BmiClassification result = bmiDeterminator.DetermineBmi(bmi);
+
+        //    // assert
+
+        //    Assert.Equal(BmiClassification.Underweight, result);
+        //}
         [Theory]
         [InlineData(10.0)]
         [InlineData(13.0)]
@@ -19,6 +35,37 @@
             // assert
 
             Assert.Equal(BmiClassification.Underweight, result);
+        }
+
+        [Theory]
+        [InlineData(10.0, BmiClassification.Underweight)]
+        [InlineData(13.0, BmiClassification.Underweight)]
+        [InlineData(15.0, BmiClassification.Underweight)]
+        [InlineData(19.0, BmiClassification.Normal)]
+        [InlineData(21.0, BmiClassification.Normal)]
+        [InlineData(24.0, BmiClassification.Normal)]
+        [InlineData(25.0, BmiClassification.Overweight)]
+        [InlineData(26.0, BmiClassification.Overweight)]
+        [InlineData(28.8, BmiClassification.Overweight)]
+        [InlineData(30.9, BmiClassification.Obesity)]
+        [InlineData(31.0, BmiClassification.Obesity)]
+        [InlineData(32.8, BmiClassification.Obesity)]
+        [InlineData(36.5, BmiClassification.ExtremeObesity)]
+        [InlineData(37.2, BmiClassification.ExtremeObesity)]
+        [InlineData(39.2, BmiClassification.ExtremeObesity)]
+        public void DetermineBmi_ForGivenBmi_ReturnsCorrectClassification(double bmi, BmiClassification classification)
+        {
+            // arrange
+
+            BmiDeterminator bmiDeterminator = new BmiDeterminator();
+
+            // act
+
+            BmiClassification result = bmiDeterminator.DetermineBmi(bmi);
+
+            // assert
+
+            Assert.Equal(classification, result);
         }
     }
 }
