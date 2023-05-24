07b45e7a7c38b8c97bfbdabba3d9946e9d799d8b
TheLukCraft <luk1997r@o2.pl>
2023-05-24 06:47:54 +0200

    BMI algorithm testing

---
+++ MyProject.Tests/MetricBmiCalculatorTests.cs
@@ -0,0 +1,27 @@
+﻿namespace MyProject.Tests
+{
+    public class MetricBmiCalculatorTests
+    {
+        [Theory]
+        [InlineData(100, 170, 34.6)]
+        [InlineData(57, 170, 19.72)]
+        [InlineData(70, 170, 24.22)]
+        [InlineData(77, 160, 30.08)]
+        [InlineData(80, 190, 22.16)]
+        [InlineData(90, 190, 24.93)]
+        public void CalculateBmi_ForGivenWeightAndHeight_ReturnsCorrectBmi(double weight, double height, double bmiResult)
+        {
+            // arrange
+
+            MetricBmiCalculator calculator = new MetricBmiCalculator();
+
+            // act
+
+            double result = calculator.CalculateBmi(weight, height);
+
+            // asseret
+
+            Assert.Equal(bmiResult, result);
+        }
+    }
+}
