44c5e45b5b9ce070ac7a57dc9c9b3c910d1e566f
TheLukCraft <luk1997r@o2.pl>
2023-05-24 04:21:39 +0200

    First test

---
+++ MyProject.Tests/StringBuilderTests.cs
@@ -0,0 +1,27 @@
+using System.Text;
+
+namespace MyProject.Tests
+{
+    public class StringBuilderTests
+    {
+        [Fact]
+        public void Append_ForTwoStrings_ReturnsConcatenatedString()
+        {
+            //NazwaMetody_Scenariusz_Rezultat
+            // arrange
+
+            StringBuilder sb = new StringBuilder();
+
+            // act
+
+            sb.Append("test")
+                .Append("test2");
+
+            string result = sb.ToString();
+
+            // assert
+
+            Assert.Equal("testtest2", result);
+        }
+    }
+}
